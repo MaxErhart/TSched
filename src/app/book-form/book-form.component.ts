87395be637166bc106b3eb793957e9788f41765a
Max Erhart <merhart@web.de>
2020-09-03 17:28:08 +0200

    fletched out book-form

--- src/app/book-form/book-form.component.ts
+++ src/app/book-form/book-form.component.ts
@@ -2,6 +2,33 @@ import { Component, OnInit, HostListener } from '@angular/core';
 import {FormControl, FormGroup, Validators, ValidatorFn, ValidationErrors, FormGroupDirective, NgForm, AbstractControl} from '@angular/forms';
 import * as Moment from 'moment';
 
+export const invalidTimeSpanValidator: ValidatorFn = (control: FormGroup): ValidationErrors | null => {
+	const startTime = control.get('startTime');
+	const endTime = control.get('endTime');
+	if(startTime.value && endTime.value){
+		if(+startTime.value.split(':')[0] > +endTime.value.split(':')[0] || (+startTime.value.split(':')[0] == +endTime.value.split(':')[0] && +startTime.value.split(':')[1] >= +endTime.value.split(':')[1])){
+			control.get('startTime').setErrors({startTimeLarger: true});
+			control.get('endTime').setErrors({startTimeLarger: true});
+			return {startTimeLarger: true};
+
+		} else {
+			control.get('startTime').setErrors(null);
+			control.get('endTime').setErrors(null);
+			return null;
+		}
+	}
+};
+
+
+export const pastDateValidator: ValidatorFn = (control: FormControl): ValidationErrors | null => {
+	const today = new Date();
+	if(control.value && control.value._d.getFullYear()>=today.getFullYear() && control.value._d.getMonth()>=today.getMonth() && control.value._d.getDate()>=today.getDate()){
+		return null;
+	} else {
+		return {pastDate: true};
+	}
+}
+
 @Component({
   selector: 'app-book-form',
   templateUrl: './book-form.component.html',
@@ -13,15 +40,19 @@ export class BookFormComponent implements OnInit {
 	colors = {Grau: 'gray', Lila: '#6300ee', Grün: '#03dac4', Blau: '#58949C', Rot: '#F9665E', Pink: '#E18AAA'};
 	page2Ative = false;
 	active = true;
-	bookForm = new FormGroup({
-		date: new FormControl(''),
-		startTime: new FormControl(''),
-		endTime: new FormControl(''),
-		weekly: new FormControl(''),
-		title: new FormControl(''),
-		color: new FormControl(''),
-		info: new FormControl(''),
-	});
+	errorMessages = {startTimeLarger: 'Zeitspanne ungültig', pastDate: 'Datum veraltet', required: 'Benötigtes Feld'}
+	bookForm = new FormGroup(
+		{
+			date: new FormControl('', [Validators.required, pastDateValidator]),
+			startTime: new FormControl('', [Validators.required, Validators.pattern('([01]?[0-9]|20):(00|15|30|45)')]),
+			endTime: new FormControl('', [Validators.required, Validators.pattern('([01]?[0-9]|20):(00|15|30|45)')]),
+			weekly: new FormControl(''),
+			title: new FormControl('', [Validators.required]),
+			color: new FormControl(''),
+			info: new FormControl(''),
+		},
+		{ validators: invalidTimeSpanValidator },
+	);
 
   constructor() { }
 
@@ -29,10 +60,18 @@ export class BookFormComponent implements OnInit {
   }
 
   @HostListener('document:click', ['$event']) documentClick(event: MouseEvent) {
+  	// console.log(this.bookForm)
+  	// console.log(Object.keys(this.bookForm.get('date').errors))
   }
 
   openPage2(){
-  	this.page2Ative=true;
+  	if(this.bookForm.get('date').errors == null && this.bookForm.get('startTime').errors == null && this.bookForm.get('endTime').errors == null){
+  		this.page2Ative=true;
+  	} else {
+	    this.bookForm.get('date').markAsTouched();
+	    this.bookForm.get('startTime').markAsTouched();
+	    this.bookForm.get('endTime').markAsTouched();
+  	}
   }
 
   closePage2(){
@@ -40,10 +79,20 @@ export class BookFormComponent implements OnInit {
   }
 
   close(submit){
-  	this.active = false;
   	if(submit){
-
+	  	if(this.bookForm.valid){
+		  	this.active = false;
+	  	}
+  	} else {
+  		this.active = false;
   	}
+  }
+
+  getErrorMessage(control){
+  	if(control.errors){
+	  	return this.errorMessages[Object.keys(control.errors)[0]];
+  	}
+
   }
 
 }
